e91bec02dacfe3b06c647c8dc82894c551310661
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2018-11-10 20:13:07 -0500

    begin working with Oauth

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
+import os
 from flask import render_template
 from app import app
+from quickbooks import Oauth2SessionManager 
+
+callback_url = 'http://localhost:5000'
+
+@app.route('/auth')
+def auth():
+    session_manager = Oauth2SessionManager(
+        client_id = os.environ['QUICKBOOKS_CLIENT_ID'],
+        client_secret = os.environ['QUICKBOOKS_CLIENT_SECRET'],
+        base_url = callback_url,
+    )
+    authorize_url = session_manager.get_authorize_url(callback_url)
+    session_manager.get_access_tokens(request.GET['code'])
+    access_token = session_manager.access_token
+    refresh_token = session_manager.refresh_token
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home') 
+    return "Hello World"
