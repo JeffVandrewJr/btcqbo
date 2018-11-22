124aab82519b67c44e392a0c4209c8b894c6ffc4
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2018-11-22 13:24:44 -0500

    remove unnecessary config.py entries

--- config.py
+++ config.py
@@ -4,7 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'lkjvasdfjkhvasjkhvsdlkvlksdvnjksdnvjngbkmdhjvnlkdjkkflg'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
