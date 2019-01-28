5010c1c955df8961b861766a4fa226854a07059d
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-01-28 17:42:28 -0500

    logging

--- app/routes.py
+++ app/routes.py
@@ -143,7 +143,7 @@ def setmail():
 def paymentapi():
     # receives and processes invoice notifications from BTCPay
     if not request.json or 'id' not in request.json:
-        app.logger.error(f'No JSON in POST: {request.text}')
+        app.logger.error(f'No JSON in POST: {request}')
         return 'No JSON in Post', 400
     btc_client = fetch('btc_client')
     invoice = btc_client.get_invoice(request.json['id'])
@@ -192,8 +192,8 @@ def paymentapi():
 def deposit_api():
     # receives and processes deposit notifications from BTCPay
     if not request.json or 'id' not in request.json:
-        app.logger.error(f'No JSON in POST: {request.text}')
-        return 'No JSON in Post', 400 
+        app.logger.error(f'No JSON in POST: {request}')
+        return 'No JSON in Post', 400
     forward_url = fetch('forward_url')
     if forward_url is not None and forward_url != '':
         r = requests.post(forward_url, json=request.get_json())
